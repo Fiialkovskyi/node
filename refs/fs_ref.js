05d6227113b89d013a32bf63030373bbfb7afbb8
Fiialkovskyi <rapgame1992@yahoo.com>
2021-05-16 18:57:06 +0300

    api and console

--- refs/fs_ref.js
+++ refs/fs_ref.js
@@ -8,34 +8,12 @@ const path = require('path');
 //     console.log('dir was created')
 // })
 
-// fs.writeFile(path.join(__dirname, 'notes', 'mynotes.txt'), 'hellooo s sad sad ss world', (err) => {
-//     if (err) {
-//         throw err;
-//     } else {
-//         console.log('the file was created');
-//         fs.appendFile(path.join(__dirname, 'notes', 'mynotes.txt'), 
-//         '--- new note ---', 
-//         (err) => {
-//             if (err) throw err;
-//             console.log('file was changed');
-//         })
+fs.writeFile(path.join(__dirname, 'notes', 'mynotes.txt'), 'hellooo sss world', (err) => {
+    if (err) {
+        throw err;
+    } else {
+        console.log('the file was created');
+    }
 
-//         fs.readFile(
-//             path.join(__dirname, 'notes', 'mynotes.txt'),
-//             'utf-8',
-//             (err, data) => {
-//                 if (err) throw err;
-//                 console.log(data);
-//             }
-//         )
-//     }
-// });
-
-fs.rename(
-    path.join(__dirname, 'notes', 'mynotes.txt'),
-    path.join(__dirname, 'notes', 'notes.txt'),
-    (err) => {
-        if (err) throw err;
-        console.log('renamed');
-    } 
-)
+    
+});
